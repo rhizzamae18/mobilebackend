50d6135907fd353cc5bd12d90313f1fd85ef66c8
rhizzamae18 <rhizzadiscaya18@gmail.com>
2025-07-19 16:33:59 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,19 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "user", // default user for XAMPP
-  password: "dialieasepass", // empty by default in XAMPP
-  database: "capd", // change to your DB name
+  host: process.env.DB_HOST,        // e.g., 'localhost'
+  user: process.env.DB_USER,        // e.g., 'user'
+  password: process.env.DB_PASSWORD,// e.g., 'dialieasepass'
+  database: process.env.DB_NAME     // e.g., 'capd'
 });
 
 connection.connect((err) => {
-  if (err) throw err;
-  console.log("Connected to MySQL Database");
+  if (err) {
+    console.error("Database connection failed:", err.stack);
+    return;
+  }
+  console.log("Connected to MySQL database.");
 });
 
 module.exports = connection;
