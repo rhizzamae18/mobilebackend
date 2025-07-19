ed006b820f29d7a3a794b6b83c3f1b33a4495b80
rhizzamae18 <rhizzadiscaya18@gmail.com>
2025-07-19 18:30:07 +0800

    Update test-db.js

--- test-db.js
+++ test-db.js
@@ -4,11 +4,11 @@ const mysql = require("mysql2/promise");
 (async () => {
   try {
     const pool = mysql.createPool({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    });
+  host: '127.0.0.1', // Force IPv4 (instead of 'localhost')
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
     const connection = await pool.getConnection();
     console.log("✅ Successfully connected to MySQL!");
