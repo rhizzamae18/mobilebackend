7c0bd10ff55437f73e31ec11b350261624dadf75
rhizzamae18 <rhizzadiscaya18@gmail.com>
2025-07-21 13:58:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 // MySQL connection pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
+  host: "127.0.0.1",
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
